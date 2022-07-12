1241b933c1ad45472537a5cb6629caba5cd54b3e
Peter Hill <peter.hill@york.ac.uk>
2022-07-12 15:12:24 +0100

    Merge deposits module into record module

--- tdotdat/records/api.py
+++ tdotdat/records/api.py
@@ -7,6 +7,12 @@
 
 """Records API."""
 
+import uuid
+
+from invenio_db import db
+from invenio_indexer.api import RecordIndexer
+from invenio_pidstore import current_pidstore
+from invenio_jsonschemas import current_jsonschemas
 from invenio_records_files.api import Record as FilesRecord
 
 
@@ -14,3 +20,31 @@ class Record(FilesRecord):
     """Custom record."""
 
     _schema = "records/record-v1.0.0.json"
+
+    @classmethod
+    def create(cls, data, id_=None, **kwargs):
+        data["$schema"] = current_jsonschemas.path_to_url(cls._schema)
+
+        if "equilibrium_id" in data:
+            data["equilibrium"] = {
+                "$ref": f"http://tdotdat.com/resolver/equilibrium/{data['equilibrium_id']}"
+            }
+
+        return super().create(data, id_=id_, **kwargs)
+
+
+def create_record(data):
+    """Create a record.
+
+    :param dict data: The record data.
+    """
+    with db.session.begin_nested():
+        # create uuid
+        rec_uuid = uuid.uuid4()
+        # create PID
+        current_pidstore.minters["recid"](rec_uuid, data)
+        # create record
+        created_record = Record.create(data, id_=rec_uuid)
+        # index the record
+        RecordIndexer().index(created_record)
+    db.session.commit()
