59fae486c6dcccdb25763fa51929690a8e3ac40e
Peter Hill <peter.hill@york.ac.uk>
2022-07-12 16:39:00 +0100

    Use jsonresolver hostname from config

--- tdotdat/records/jsonresolvers.py
+++ tdotdat/records/jsonresolvers.py
@@ -1,16 +1,16 @@
 import jsonresolver
 from invenio_pidstore.resolver import Resolver
 from tdotdat.records.api import Record
+from ..config import JSONSCHEMAS_HOST
 
 
-# the host corresponds to the config value for the key JSONSCHEMAS_HOST
-@jsonresolver.route('/api/resolver/equilibrium/<equid>', host='tdotdat.com')
+@jsonresolver.route("/api/resolver/equilibrium/<equid>", host=JSONSCHEMAS_HOST)
 def record_jsonresolver(equid):
     """Resolve referenced equilibrium."""
     # Setup a resolver to retrieve an equilibrium record given its id
-    resolver = Resolver(pid_type='equid', object_type="rec", getter=Record.get_record)
+    resolver = Resolver(pid_type="equid", object_type="rec", getter=Record.get_record)
     _, record = resolver.resolve(str(equid))
     # Get rid of bits we don't want
-    del record['$schema']
+    del record["$schema"]
     del record["_bucket"]
     return record
