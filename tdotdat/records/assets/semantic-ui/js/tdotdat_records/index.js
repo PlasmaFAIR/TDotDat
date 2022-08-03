9af9cc71b4e57b72d0925d0305fe6a9440971caf
Peter Hill <peter.hill@york.ac.uk>
2022-08-03 11:17:21 +0100

    Move search results list/grid items customisations to same file

--- tdotdat/records/assets/semantic-ui/js/tdotdat_records/index.js
+++ tdotdat/records/assets/semantic-ui/js/tdotdat_records/index.js
@@ -1,9 +1,13 @@
 import { createSearchAppInit } from "@js/invenio_search_ui";
-import { TDotDatSearchBarElement } from "./search_app_customizations";
+import {
+  TDotDatSearchBarElement,
+  TDotDatResultsListItem,
+  TDotDatResultsGridItem,
+ } from "./search_app_customizations";
 
 const initSearchApp = createSearchAppInit({
-  "ResultsList.item": null, // Just pass null to overwrite it with the JSX components declared in the templates/search folder
-  "ResultsGrid.item": null,
+  "ResultsList.item": TDotDatResultsListItem,
+  "ResultsGrid.item": TDotDatResultsGridItem,
   "SearchBar.element": TDotDatSearchBarElement,
   "SearchApp.searchbarContainer": () => null,
 });
--- tdotdat/records/assets/semantic-ui/js/tdotdat_records/search_app_customizations.js
+++ tdotdat/records/assets/semantic-ui/js/tdotdat_records/search_app_customizations.js
@@ -6,7 +6,9 @@
  */
 
 import React from "react";
-import { Input } from "semantic-ui-react";
+import { Input, Item, List, Checkbox, Card, Form, Button, Grid } from "semantic-ui-react";
+import { SearchBar } from "react-searchkit";
+
 
 export const TDotDatSearchBarElement = ({
   placeholder: passedPlaceholder,
@@ -25,20 +27,69 @@ export const TDotDatSearchBarElement = ({
   };
   return (
     <Input
-      action={{
-        icon: "search",
-        onClick: onBtnSearchClick,
-        color: "orange",
-        className: "invenio-theme-search-button",
-      }}
-      placeholder={placeholder}
-      onChange={(event, { value }) => {
-        onInputChange(value);
-      }}
-      value={queryString}
-      onKeyPress={onKeyPress}
+        action={{
+          icon: "search",
+          onClick: onBtnSearchClick,
+          color: "orange",
+          className: "invenio-theme-search-button",
+        }}
+        placeholder={placeholder}
+        onChange={(event, { value }) => {
+          onInputChange(value);
+        }}
+        value={queryString}
+        onKeyPress={onKeyPress}
     />
   );
 };
 
+export const TDotDatResultsListItem = ({ result, index }) => {
+  console.log("list item: ", result, index);
 
+  const contributors = result.metadata.contributors || [];
+
+  return (
+    <Item key={index}>
+      <Item.Content>
+        <Item.Header href={`/records/${result.id}`}>{result.metadata.title}</Item.Header>
+        <Item.Description>
+          <p>Run with <b>{result.metadata.software.name}</b></p>
+          {contributors && (
+            <List horizontal relaxed>
+              {contributors.map((contributor, idx) => (
+                <List.Item key={idx}>{contributor.name}</List.Item>
+              ))}
+            </List>
+          )}
+        </Item.Description>
+        <Item.Extra>
+          <Checkbox
+              label="select"
+              name='recordResultItem'
+              value={`${result.id}`}
+          />
+        </Item.Extra>
+      </Item.Content>
+    </Item>
+  );
+};
+
+export const TDotDatResultsGridItem = ({ result, index }) => {
+  const contributors = result.metadata.contributors || [];
+  return (
+    <Card fluid key={index} href={`/records/${result.id}`}>
+      <Card.Content>
+        <Card.Header>{result.metadata.title}</Card.Header>
+        <Card.Description>
+          {contributors && (
+            <List horizontal relaxed>
+              {contributors.map((contributor, idx) => (
+                <List.Item key={idx}>{contributor.name}</List.Item>
+              ))}
+            </List>
+          )}
+        </Card.Description>
+      </Card.Content>
+    </Card>
+  );
+};
